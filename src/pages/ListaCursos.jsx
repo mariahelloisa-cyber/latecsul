563f8724cb55f2a43d5edcf148a385d0c82fdaf6
mariahelloisa-cyber <maria.helloisa@facla.edu.br>
2026-08-26 17:13:58 -0300

    seduc

--- src/pages/ListaCursos.jsx
+++ src/pages/ListaCursos.jsx
@@ -156,7 +156,7 @@ export default function ListaCursos() {
     <div className="w-full min-h-screen bg-[#fafafa] text-gray-900 antialiased pb-20 flex flex-col">
       <Navbar />
 
-      <EsteiraFrases texto="Todos os cursos são reconhecido pela SEDUC" />
+      <EsteiraFrases texto="Todos os cursos são reconhecidos pela SEDUC" />
 
       {/* 1. HERO SECTION CORRIGIDA (Preenchimento total da tela sem cortes nem espaços brancos) */}
       <div 
@@ -203,7 +203,7 @@ export default function ListaCursos() {
         </div>
       </div>
 
-      <EsteiraFrases texto="Todos os cursos são reconhecido pelo MEC." />
+      <EsteiraFrases texto="Todos os cursos são reconhecidos pela SEDUC." />
 
       {/* 2. FILTROS E CONTEÚDO */}
       <div className="max-w-6xl w-full mx-auto px-6 mt-10">
